df30f4744818b4fdea6cb2de53cdfc971ba304ff
Chad Stark <chad.d.stark@hotmail.com>
2021-04-20 23:04:44 -0700

    Basic days, card, column and rows

--- src/App.js
+++ src/App.js
@@ -1,23 +1,178 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Card, Avatar } from "antd";
+import { Row, Col } from "antd";
+import "antd/dist/antd.dark.css";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import sunnyDay from "./images/sunnySky.jpg";
+import { WiDaySunny } from "react-icons/wi";
+
+const { Meta } = Card;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row>
+        <Col span={6}>
+          <Card
+            className="weather-card"
+            cover={
+              <img className="weather-image" alt="example" src={sunnyDay} />
+            }
+            // actions={[
+            //   <SettingOutlined key="setting" />,
+            //   <EditOutlined key="edit" />,
+            //   <EllipsisOutlined key="ellipsis" />,
+            // ]}
+          >
+            <Meta
+              className="bottom-of-card"
+              avatar={
+                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+              }
+              title="Monday"
+              description="Today, it is clear skies"
+            />
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card
+            className="weather-card"
+            cover={
+              <img className="weather-image" alt="example" src={sunnyDay} />
+            }
+            // actions={[
+            //   <SettingOutlined key="setting" />,
+            //   <EditOutlined key="edit" />,
+            //   <EllipsisOutlined key="ellipsis" />,
+            // ]}
+          >
+            <Meta
+              className="bottom-of-card"
+              avatar={
+                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+              }
+              title="Tuesday"
+              description="Today, it is clear skies"
+            />
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card
+            className="weather-card"
+            cover={
+              <img className="weather-image" alt="example" src={sunnyDay} />
+            }
+            // actions={[
+            //   <SettingOutlined key="setting" />,
+            //   <EditOutlined key="edit" />,
+            //   <EllipsisOutlined key="ellipsis" />,
+            // ]}
+          >
+            <Meta
+              className="bottom-of-card"
+              avatar={
+                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+              }
+              title="Wednesday"
+              description="Today, it is clear skies"
+            />
+          </Card>
+        </Col>
+        <Col span={6}>
+          <Card
+            className="weather-card"
+            cover={
+              <img className="weather-image" alt="example" src={sunnyDay} />
+            }
+            // actions={[
+            //   <SettingOutlined key="setting" />,
+            //   <EditOutlined key="edit" />,
+            //   <EllipsisOutlined key="ellipsis" />,
+            // ]}
+          >
+            <Meta
+              className="bottom-of-card"
+              avatar={
+                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+              }
+              title="Thursday"
+              description="Today, it is clear skies"
+            />
+          </Card>
+        </Col>
+      </Row>
+      <Row className="second-row">
+        <Col span={8}>
+          <Card
+            className="weather-card"
+            cover={
+              <img className="weather-image" alt="example" src={sunnyDay} />
+            }
+            // actions={[
+            //   <SettingOutlined key="setting" />,
+            //   <EditOutlined key="edit" />,
+            //   <EllipsisOutlined key="ellipsis" />,
+            // ]}
+          >
+            <Meta
+              className="bottom-of-card"
+              avatar={
+                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+              }
+              title="Friday"
+              description="Today, it is clear skies"
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            className="weather-card"
+            cover={
+              <img className="weather-image" alt="example" src={sunnyDay} />
+            }
+            // actions={[
+            //   <SettingOutlined key="setting" />,
+            //   <EditOutlined key="edit" />,
+            //   <EllipsisOutlined key="ellipsis" />,
+            // ]}
+          >
+            <Meta
+              className="bottom-of-card"
+              avatar={
+                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+              }
+              title="Saturday"
+              description="Today, it is clear skies"
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            className="weather-card"
+            cover={
+              <img className="weather-image" alt="example" src={sunnyDay} />
+            }
+            // actions={[
+            //   <SettingOutlined key="setting" />,
+            //   <EditOutlined key="edit" />,
+            //   <EllipsisOutlined key="ellipsis" />,
+            // ]}
+          >
+            <Meta
+              className="bottom-of-card"
+              avatar={
+                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+              }
+              title="Sunday"
+              description="Today, it is clear skies"
+            />
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
