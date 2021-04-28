857b155bda224d9a895b0d85fa064e4b3325653a
Chad Stark <chad.d.stark@hotmail.com>
2021-04-28 08:39:13 -0700

    Updated card component

--- src/App.js
+++ src/App.js
@@ -9,170 +9,181 @@ import {
 } from "@ant-design/icons";
 import sunnyDay from "./images/sunnySky.jpg";
 import { WiDaySunny } from "react-icons/wi";
+import { Layout } from "antd";
 
 const { Meta } = Card;
 
 function App() {
   return (
     <div className="App">
-      <Row>
-        <Col span={6}>
-          <Card
-            className="weather-card"
-            cover={
-              <img className="weather-image" alt="example" src={sunnyDay} />
-            }
-            // actions={[
-            //   <SettingOutlined key="setting" />,
-            //   <EditOutlined key="edit" />,
-            //   <EllipsisOutlined key="ellipsis" />,
-            // ]}
-          >
-            <Meta
-              className="bottom-of-card"
-              avatar={
-                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+      <div className="both-rows">
+        <Row>
+          <Col span={6}>
+            <Card
+              className="weather-card grow"
+              // cover={
+              //   <img className="weather-image" alt="example" src={sunnyDay} />
+              // }
+              // // actions={[
+              // //   <SettingOutlined key="setting" />,
+              // //   <EditOutlined key="edit" />,
+              // //   <EllipsisOutlined key="ellipsis" />,
+              // // ]}
+              onClick={() => alert("hello World")}
+            >
+              <Row>
+                <Col span={12}>Temperature here</Col>
+                <Col span={12} className="vl">
+                  picture here
+                </Col>
+              </Row>
+              <hr></hr>
+              <Meta
+                className="bottom-of-card"
+                avatar={
+                  <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+                }
+                title="Monday"
+                description="Today, it is clear skies"
+              />
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card
+              className="weather-card grow"
+              cover={
+                <img className="weather-image" alt="example" src={sunnyDay} />
               }
-              title="Monday"
-              description="Today, it is clear skies"
-            />
-          </Card>
-        </Col>
-        <Col span={6}>
-          <Card
-            className="weather-card"
-            cover={
-              <img className="weather-image" alt="example" src={sunnyDay} />
-            }
-            // actions={[
-            //   <SettingOutlined key="setting" />,
-            //   <EditOutlined key="edit" />,
-            //   <EllipsisOutlined key="ellipsis" />,
-            // ]}
-          >
-            <Meta
-              className="bottom-of-card"
-              avatar={
-                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+              // actions={[
+              //   <SettingOutlined key="setting" />,
+              //   <EditOutlined key="edit" />,
+              //   <EllipsisOutlined key="ellipsis" />,
+              // ]}
+            >
+              <Meta
+                className="bottom-of-card"
+                avatar={
+                  <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+                }
+                title="Tuesday"
+                description="Today, it is clear skies"
+              />
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card
+              className="weather-card grow"
+              cover={
+                <img className="weather-image" alt="example" src={sunnyDay} />
               }
-              title="Tuesday"
-              description="Today, it is clear skies"
-            />
-          </Card>
-        </Col>
-        <Col span={6}>
-          <Card
-            className="weather-card"
-            cover={
-              <img className="weather-image" alt="example" src={sunnyDay} />
-            }
-            // actions={[
-            //   <SettingOutlined key="setting" />,
-            //   <EditOutlined key="edit" />,
-            //   <EllipsisOutlined key="ellipsis" />,
-            // ]}
-          >
-            <Meta
-              className="bottom-of-card"
-              avatar={
-                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+              // actions={[
+              //   <SettingOutlined key="setting" />,
+              //   <EditOutlined key="edit" />,
+              //   <EllipsisOutlined key="ellipsis" />,
+              // ]}
+            >
+              <Meta
+                className="bottom-of-card"
+                avatar={
+                  <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+                }
+                title="Wednesday"
+                description="Today, it is clear skies"
+              />
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card
+              className="weather-card grow"
+              cover={
+                <img className="weather-image" alt="example" src={sunnyDay} />
               }
-              title="Wednesday"
-              description="Today, it is clear skies"
-            />
-          </Card>
-        </Col>
-        <Col span={6}>
-          <Card
-            className="weather-card"
-            cover={
-              <img className="weather-image" alt="example" src={sunnyDay} />
-            }
-            // actions={[
-            //   <SettingOutlined key="setting" />,
-            //   <EditOutlined key="edit" />,
-            //   <EllipsisOutlined key="ellipsis" />,
-            // ]}
-          >
-            <Meta
-              className="bottom-of-card"
-              avatar={
-                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+              // actions={[
+              //   <SettingOutlined key="setting" />,
+              //   <EditOutlined key="edit" />,
+              //   <EllipsisOutlined key="ellipsis" />,
+              // ]}
+            >
+              <Meta
+                className="bottom-of-card"
+                avatar={
+                  <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+                }
+                title="Thursday"
+                description="Today, it is clear skies"
+              />
+            </Card>
+          </Col>
+        </Row>
+        <Row className="second-row">
+          <Col span={6}>
+            <Card
+              className="weather-card weather-card-3 grow"
+              cover={
+                <img className="weather-image" alt="example" src={sunnyDay} />
               }
-              title="Thursday"
-              description="Today, it is clear skies"
-            />
-          </Card>
-        </Col>
-      </Row>
-      <Row className="second-row">
-        <Col span={8}>
-          <Card
-            className="weather-card"
-            cover={
-              <img className="weather-image" alt="example" src={sunnyDay} />
-            }
-            // actions={[
-            //   <SettingOutlined key="setting" />,
-            //   <EditOutlined key="edit" />,
-            //   <EllipsisOutlined key="ellipsis" />,
-            // ]}
-          >
-            <Meta
-              className="bottom-of-card"
-              avatar={
-                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+              // actions={[
+              //   <SettingOutlined key="setting" />,
+              //   <EditOutlined key="edit" />,
+              //   <EllipsisOutlined key="ellipsis" />,
+              // ]}
+            >
+              <Meta
+                className="bottom-of-card"
+                avatar={
+                  <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+                }
+                title="Friday"
+                description="Today, it is clear skies"
+              />
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card
+              className="weather-card weather-card-3 grow"
+              cover={
+                <img className="weather-image" alt="example" src={sunnyDay} />
               }
-              title="Friday"
-              description="Today, it is clear skies"
-            />
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card
-            className="weather-card"
-            cover={
-              <img className="weather-image" alt="example" src={sunnyDay} />
-            }
-            // actions={[
-            //   <SettingOutlined key="setting" />,
-            //   <EditOutlined key="edit" />,
-            //   <EllipsisOutlined key="ellipsis" />,
-            // ]}
-          >
-            <Meta
-              className="bottom-of-card"
-              avatar={
-                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+              // actions={[
+              //   <SettingOutlined key="setting" />,
+              //   <EditOutlined key="edit" />,
+              //   <EllipsisOutlined key="ellipsis" />,
+              // ]}
+            >
+              <Meta
+                className="bottom-of-card"
+                avatar={
+                  <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+                }
+                title="Saturday"
+                description="Today, it is clear skies"
+              />
+            </Card>
+          </Col>
+          <Col span={6}>
+            <Card
+              className="weather-card weather-card-3 grow"
+              cover={
+                <img className="weather-image" alt="example" src={sunnyDay} />
               }
-              title="Saturday"
-              description="Today, it is clear skies"
-            />
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card
-            className="weather-card"
-            cover={
-              <img className="weather-image" alt="example" src={sunnyDay} />
-            }
-            // actions={[
-            //   <SettingOutlined key="setting" />,
-            //   <EditOutlined key="edit" />,
-            //   <EllipsisOutlined key="ellipsis" />,
-            // ]}
-          >
-            <Meta
-              className="bottom-of-card"
-              avatar={
-                <Avatar src={<WiDaySunny className="small-weather-icon" />} />
-              }
-              title="Sunday"
-              description="Today, it is clear skies"
-            />
-          </Card>
-        </Col>
-      </Row>
+              // actions={[
+              //   <SettingOutlined key="setting" />,
+              //   <EditOutlined key="edit" />,
+              //   <EllipsisOutlined key="ellipsis" />,
+              // ]}
+            >
+              <Meta
+                className="bottom-of-card"
+                avatar={
+                  <Avatar src={<WiDaySunny className="small-weather-icon" />} />
+                }
+                title="Sunday"
+                description="Today, it is clear skies"
+              />
+            </Card>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
