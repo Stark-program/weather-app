4ce6ba2b5aee526f29119f7fd47362930679a645
Chad Stark <chad.d.stark@hotmail.com>
2021-05-03 22:46:03 -0700

    added google api and weather api functionality

--- src/location.js
+++ src/location.js
@@ -1,17 +1,26 @@
 const NodeGeocoder = require("node-geocoder");
+const axios = require("axios").default;
+require("dotenv").config();
 
 const options = {
   provider: "google",
-  apiKey: "AIzaSyDm88CgE7BiYzsiWG77_DhonqhVc6slEOo",
+  apiKey: process.env.GOOGLE_API_KEY,
 };
 const geocoder = NodeGeocoder(options);
 
 const res = async function test() {
-  const result = await geocoder.geocode("Littleton, CO");
+  const api_key = process.env.WEATHER_API_KEY;
+  const result = await geocoder.geocode("Bishop, CA");
   const lat = result[0].latitude;
   const long = result[0].longitude;
+  console.log(result, lat, long);
 
-  return console.log(result, lat, long);
+  const weather = axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&exclude=hourly,daily,minutely{part}&appid=${api_key}`
+    )
+    .then((response) => console.log(response.data));
+  console.log(weather);
 };
 
 res();
