f6eeae150c59258e616b109732d0862ff8c07146
Chad Stark <chad.d.stark@hotmail.com>
2021-05-20 15:13:11 -0700

    fixed some css bugs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { Route } from "react-router-dom";
-import Details from "./Pages/expanded_details";
+
 import Days from "./Pages/weather_days";
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Days} />
-      <Route exact path="/Pages/expanded_details" component={Details} />
     </div>
   );
 }
